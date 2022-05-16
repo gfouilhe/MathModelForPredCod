20552bffc515475eb086097fc198df3950e1dd0f
Guilhem Fouilhé <g.g.a.fouilhe@gmail.com>
2022-05-16 17:56:36 +0200

    update

--- MNIST-14x14-3H/getMatrices.py
+++ MNIST-14x14-3H/getMatrices.py
@@ -20,16 +20,24 @@ for name, p in model.named_parameters():
         Wab = p.detach().numpy()
     if name=='fcBA.weight':
         Wba = p.detach().numpy()
+    if name=='fcBC.weight':
+        Wbc = p.detach().numpy()
+    if name=='fcCB.weight':
+        Wcb = p.detach().numpy()
 
-d = 120
-Wab = np.eye(d)
-Wba = np.eye(d)
+d = 196
 
-A11 = (1-beta-gamma) * np.eye(d)
+
+A11 = (1-beta-gamma) * np.eye(5)
 A12 = beta * Wba
-A21 = (1-beta-gamma) * gamma * Wab + alpha/d * Wba.T
-A22 = gamma * beta * Wab.dot(Wba) + (1-gamma) * np.eye(d) - alpha/d * Wba.T.dot(Wba)
-A = np.block([[A11,A12],[A21,A22]])
+A13 = np.zeros((5,5))
+A21 = (1-beta-gamma) * gamma * Wba.T + alpha/5 * Wba.T
+A22 = gamma * beta * Wab.dot(Wba) + (1-beta-gamma) * np.eye(5) - alpha/5 * Wba.T.dot(Wba)
+A23 = beta * Wcb
+A31 = (1-beta-gamma) * gamma**2 * Wbc.dot(Wab) + alpha/5 * gamma * Wbc.dot(Wcb.T)
+A32 = beta * gamma **2 * Wab.dot(Wba) + (1-beta-gamma) * gamma * Wbc - alpha/5 * gamma * Wbc.dot(Wba.T.dot(Wba)) + alpha/5 * Wcb.T
+A33 = beta * gamma * Wbc * Wcb + (1-gamma) * np.eye(5) - alpha/5 * Wcb.T.dot(Wcb)
+A = np.block([[A11,A12,A13],[A21,A22,A23],[A31,A32,A33]])
 
 w, v = np.linalg.eig(A)
 plt.figure()
