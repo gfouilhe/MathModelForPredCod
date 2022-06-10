b37a52f839dcc3a73df315676983a1520f24f129
Guilhem Fouilhé <g.g.a.fouilhe@gmail.com>
2022-06-10 10:57:31 +0200

    update

--- MNIST-14x14-3H-clean/utils/getAcc.py
+++ MNIST-14x14-3H-clean/utils/getAcc.py
@@ -3,34 +3,39 @@ import os
 
 def getMean(acc,mode='ff'):
 
-    assert mode in ['ff','fb']
+    assert mode in ['ff','fb','rec']
     if mode=='ff':
         mean = np.mean(acc[-1,:])
         return mean
-    
+    if mode=='rec':
+        mean = np.mean(acc[:,-1,:],axis=1)
+        # mean = acc
+        return mean
+
+
     timeSteps = acc.shape[0]
     mean = np.zeros(timeSteps)
     for t in range(timeSteps):
         mean[t] = np.mean(acc[t,-1,:])
     return mean
 
-path = os.path.join('accuracies','ACaccTrainingCE__G0.6_B0.2_A0.01.npy')
+path = os.path.join('accuracies','REC__G0.95_B0.01_A0.01.npy')
 acc = np.load(path)
 
 
 
-print('Mean FB : ', getMean(acc,mode='fb'))
+print('Mean All : ', getMean(acc,mode='rec')[0])
 
-path = os.path.join('accuracies','accTrainingCET__G0.6_B0.2_A0.01.npy')
-acc = np.load(path)
-
-
-
-print('Mean FB : ', getMean(acc,mode='fb'))
-
-
-
-# path = os.path.join('accuracies','accTrainingFFCE.npy')
+# path = os.path.join('accuracies','accTrainingCET__G0.6_B0.2_A0.01.npy')
 # acc = np.load(path)
 
-# print('Mean FF : ', getMean(acc))
+
+
+# print('Mean FB : ', getMean(acc,mode='fb'))
+
+
+
+# # path = os.path.join('accuracies','accTrainingFFCE.npy')
+# # acc = np.load(path)
+
+# # print('Mean FF : ', getMean(acc))
