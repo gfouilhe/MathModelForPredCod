9e00c7a46269b8a0fa84a6f80af4881e398fad11
Guilhem Fouilhé <g.g.a.fouilhe@gmail.com>
2022-06-20 17:52:44 +0200

    update scalar

--- unidimensional/TrainRec.py
+++ unidimensional/TrainRec.py
@@ -1,4 +1,3 @@
-from math import gamma
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -7,46 +6,42 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 import numpy
+from get_data import Dataset, get_data
 
-from model import PCMLP
-from utils.getdata import get_data
+from network import UniDimModel
 
 import os
 
 
-gammaFw = 0.95
+
+betaFw = 0.6
 alphaRec = 0.01
-betaFB = 0.01
+lambdaBw = 0.2
 
 
 iterationNumber = 1
 numberEpochs = 20
-activation_function = torch.tanh
-transpose = False
-complex_valued = False
-checkpoint = [f"FF_E{numberEpochs-1}_I{it}_G{gammaFw}_B{betaFB}_A{alphaRec}.pth" for it in range(iterationNumber)]
+timeSteps = 30
+checkpoint = [f"FF_E{numberEpochs-1}_I{it}_B{betaFw}_L{lambdaBw}_A{alphaRec}.pth" for it in range(iterationNumber)]
 
 
-def main(gammaFw,betaFB,alphaRec,iterationNumber,numberEpochs, activation_function, transpose, complex_valued, checkpoint):
-
-    assert checkpoint != 0, 'Reconstuction should be used with a checkpoint from FF learned weights'
+def main(betaFw,lambdaBw,alphaRec,iterationNumber,numberEpochs,timeSteps, checkpoint):
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(device)
-    print('Parameters :',gammaFw,betaFB,alphaRec,iterationNumber,numberEpochs,activation_function, transpose, complex_valued)
+    print('Parameters :',betaFw,lambdaBw,alphaRec,iterationNumber,numberEpochs,timeSteps)
         
     batchSize = 100
 
-    dataset_train,dataset_test = get_data()
+    dataset_train,dataset_test = get_data(10000)
 
 
-    # print('Train dataset size :', len(dataset_train))
-    # print('Test dataset size :', len(dataset_test))
+    print('Train dataset size :', len(dataset_train))
+    print('Test dataset size :', len(dataset_test))
 
 
     train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=batchSize, shuffle=True, num_workers=1)
     test_loader = torch.utils.data.DataLoader(dataset_test, batch_size=batchSize, shuffle=False, num_workers=1)
-
 
     memory = 0.33
 
@@ -55,7 +50,7 @@ def main(gammaFw,betaFB,alphaRec,iterationNumber,numberEpochs, activation_functi
 
     for iterationIndex in range(0, iterationNumber):
 
-        pcNet = PCMLP(memory,alphaRec,betaFB,gammaFw,transpose,complex_valued,activation_function).to(device)
+        pcNet = UniDimModel(memory,alphaRec,betaFw,lambdaBw).to(device)
         if checkpoint != 0:
             checkpointPhase = torch.load(os.path.join('models',checkpoint[iterationIndex]))
             pcNet.load_state_dict(checkpointPhase["module"])
@@ -71,42 +66,35 @@ def main(gammaFw,betaFB,alphaRec,iterationNumber,numberEpochs, activation_functi
             #train
             for i, data in enumerate(train_loader, 0):
 
-                if complex_valued:
-                    aTemp = torch.zeros(batchSize, 196).to(dtype=torch.complex64).cuda()
-                    bTemp = torch.zeros(batchSize, 196).to(dtype=torch.complex64).cuda()
-                    oTemp = torch.zeros(batchSize, 10).to(dtype=torch.complex64).cuda()
-                else:
-                    aTemp = torch.zeros(batchSize, 196).to(dtype=torch.float32).cuda()
-                    bTemp = torch.zeros(batchSize, 196).to(dtype=torch.float32).cuda()
-                    oTemp = torch.zeros(batchSize, 10).to(dtype=torch.float32).cuda()
-
+                a1Temp = torch.randn(batchSize, 1).to(dtype=torch.float32).cuda()
+                a2Temp = torch.randn(batchSize, 1).to(dtype=torch.float32).cuda()
+                a3Temp = torch.randn(batchSize, 1).to(dtype=torch.float32).cuda()
                 inputs, labels = data
-                if complex_valued:
-                    inputs, labels = inputs.to(dtype=torch.complex64).to(device),labels.to(device)
-                else:
-                    inputs, labels = inputs.to(dtype=torch.float32).to(device),labels.to(device)
-
+               
+                inputs, labels = inputs.to(dtype=torch.float32).to(device),labels.to(dtype=torch.float32).to(device)
                 optimizerPCnet.zero_grad()
                 finalLoss = 0
 
-                aTemp.requires_grad = True
-                bTemp.requires_grad = True
-                oTemp.requires_grad = True
+                a1Temp.requires_grad = True
+                a2Temp.requires_grad = True
+                a3Temp.requires_grad = True
 
-                outputs, iTemp, aTemp, bTemp, oTemp, reconstruction = pcNet(inputs.view(batchSize,-1), aTemp, bTemp, oTemp, 'forward')
-                outputs, iR, aR, bR, oR, reconstruction = pcNet(inputs.view(batchSize,-1), aTemp, bTemp, oTemp, 'reconstruction')
+                outputs, a0Temp, a1Temp, a2Temp, a3Temp, reconstruction0, reconstruction1, reconstruction2 = pcNet(inputs.view(batchSize,-1), a1Temp, a2Temp, a3Temp, 'forward')
+                
+                outputs, a0R, a1R, a2R, a3R, reconstruction0, reconstruction1, reconstruction2 = pcNet(inputs.view(batchSize,-1), a1Temp, a2Temp, a3Temp, 'reconstruction')
 
-                lossA = criterionMSE(inputs.view(batchSize,-1), iR)
-                lossB = criterionMSE(aTemp.view(batchSize,-1), aR)
+                lossA = criterionMSE(a0Temp.view(batchSize,-1), a0R.view(batchSize,-1))
+                lossB = criterionMSE(a1Temp.view(batchSize,-1), a1R.view(batchSize,-1))
+                lossC = criterionMSE(a2Temp.view(batchSize,-1), a2R.view(batchSize,-1))
 
-                finalLoss = lossA + lossB
+                finalLoss = lossA + lossB + lossC
 
                 finalLoss.backward(retain_graph=True)  
                 optimizerPCnet.step()
 
             if epoch == numberEpochs - 1 :
 
-                path = os.path.join('models',f"FFREC_E{epoch}_I{iterationIndex}_G{gammaFw}_B{betaFB}_A{alphaRec}.pth")
+                path = os.path.join('models',f"FFREC_E{epoch}_I{iterationIndex}_B{betaFw}_L{lambdaBw}_A{alphaRec}.pth")
                 torch.save({"module": pcNet.state_dict(), "epoch": epoch}, path)
 
             #compute test Loss/Acc
@@ -114,51 +102,40 @@ def main(gammaFw,betaFB,alphaRec,iterationNumber,numberEpochs, activation_functi
             total = 0
             finalLossA = 0
             finalLossB = 0
+            finalLossC = 0
             for _, data in enumerate(test_loader, 0):
 
-                if complex_valued:
-                    aTemp = torch.zeros(batchSize, 196).to(dtype=torch.complex64).cuda()
-                    bTemp = torch.zeros(batchSize, 196).to(dtype=torch.complex64).cuda()
-                    oTemp = torch.zeros(batchSize, 10).to(dtype=torch.complex64).cuda()
-                else:
-                    aTemp = torch.zeros(batchSize, 196).to(dtype=torch.float32).cuda()
-                    bTemp = torch.zeros(batchSize, 196).to(dtype=torch.float32).cuda()
-                    oTemp = torch.zeros(batchSize, 10).to(dtype=torch.float32).cuda()
-
-                aTemp.requires_grad = True
-                bTemp.requires_grad = True
-                oTemp.requires_grad = True
-
-                # get the inputs; data is a list of [inputs, labels]
+                a1Temp = torch.randn(batchSize, 1).to(dtype=torch.float32).cuda()
+                a2Temp = torch.randn(batchSize, 1).to(dtype=torch.float32).cuda()
+                a3Temp = torch.randn(batchSize, 1).to(dtype=torch.float32).cuda()
                 inputs, labels = data
-                if complex_valued:
-                    inputs, labels = inputs.to(dtype=torch.complex64).to(device),labels.to(device)
-                else:
-                    inputs, labels = inputs.to(dtype=torch.float32).to(device),labels.to(device)
+               
+                inputs, labels = inputs.to(dtype=torch.float32).to(device),labels.to(dtype=torch.float32).to(device)
 
-                outputs, iTemp, aTemp, bTemp, oTemp, reconstruction = pcNet(inputs.view(batchSize,-1), aTemp, bTemp, oTemp, 'forward')
-                _, iR, aR, bR, oR, reconstruction = pcNet(inputs.view(batchSize,-1), aTemp, bTemp, oTemp, 'reconstruction')
+                outputs, a0Temp, a1Temp, a2Temp, a3Temp, reconstruction0, reconstruction1, reconstruction2 = pcNet(inputs.view(batchSize,-1), a1Temp, a2Temp, a3Temp, 'forward')
+             
+                outputs, a0R, a1R, a2R, a3R, reconstruction0, reconstruction1, reconstruction2 = pcNet(inputs.view(batchSize,-1), a1Temp, a2Temp, a3Temp, 'reconstruction')
 
-                finalLossA += criterionMSE(inputs.view(batchSize,-1), iR)
-                finalLossB += criterionMSE(aTemp.view(batchSize,-1), aR)
+                finalLossA += criterionMSE(a0Temp.view(batchSize,-1), a0R.view(batchSize,-1))
+                finalLossB += criterionMSE(a1Temp.view(batchSize,-1), a1R.view(batchSize,-1))
+                finalLossC += criterionMSE(a2Temp.view(batchSize,-1), a2R.view(batchSize,-1))
 
-                total += labels.size(0)
 
-                _, predicted = torch.max(outputs.data, 1)
-                correct +=  (predicted == labels).sum().item()
-
-                total += labels.size(0)
+                predicted = torch.round(outputs)
+                correct += (predicted == labels.view(batchSize,-1)).sum().item()
+                total += batchSize
             acc = (100 * correct / total)
             print(acc)
             resAll[epoch, iterationIndex] = acc
             resRecLoss[0, epoch, iterationIndex] = finalLossA / total
             resRecLoss[1, epoch, iterationIndex] = finalLossB / total
+            resRecLoss[2, epoch, iterationIndex] = finalLossC / total
 
 
-    np.save(os.path.join('accuracies',f'REC__G{gammaFw}_B{betaFB}_A{alphaRec}.npy'),resRecLoss)
-    np.save(os.path.join('accuracies',f"ALL__G{gammaFw}_B{betaFB}_A{alphaRec}.npy"), resAll)
+    np.save(os.path.join('accuracies',f'REC__B{betaFw}_L{lambdaBw}_A{alphaRec}.npy'),resRecLoss)
+    np.save(os.path.join('accuracies',f"ALL__B{betaFw}_L{lambdaBw}_A{alphaRec}.npy"), resAll)
     print('Finished Training')
 
 
 if __name__ == "__main__":
-    main(gammaFw,betaFB,alphaRec,iterationNumber,numberEpochs,activation_function, transpose, complex_valued, checkpoint)
+    main(betaFw,lambdaBw,alphaRec,iterationNumber,numberEpochs, timeSteps, checkpoint)
