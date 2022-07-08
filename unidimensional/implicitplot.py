fc74c6bdf9321cb5499b17fbe9d511176728de76
Guilhem Fouilhé <g.g.a.fouilhe@gmail.com>
2022-07-08 17:51:31 +0200

    update

--- unidimensional/implicitplot.py
+++ unidimensional/implicitplot.py
@@ -24,30 +24,39 @@ def is_not_zero(x,tol=10**-6):
 a = 0.01
 d = 196
 b = 0.33
-l = 0
+l = 0.33
 
-step = 0.05
-nuimrange = np.arange(-20, 20, step)
-nurerange = np.arange(-20, 20, step)
-x, y = np.meshgrid(nurerange, nuimrange)
-nu = complexHD(x,y)
+# # step = 0.05
+# # nuimrange = np.arange(-20, 20, step)
+# # nurerange = np.arange(-20, 20, step)
+# # x, y = np.meshgrid(nurerange, nuimrange)
+# nu = complexHD(x,y)
 
 
-betaR = [0] #list(np.arange(0,1,0.1))
-lambdaR = list(np.arange(0,1,0.1))
-alphaR = [0.01,0.1,1]
+betaR = [0.2] #list(np.arange(0,1,0.1))
+lambdaR = [0.2] #list(np.arange(0,1,0.1))
+alphaR = [0.01] #,0.1,1]
+
+thetaR = np.linspace(0,2*np.pi,50)
+
 
 for b in betaR:
     for l in lambdaR:
         for a in alphaR:
 
+            ax = plt.gca()
+            fig = plt.gcf()
+            ax.cla()
+            circle = plt.Circle((0,0),1,color='r',fill=False)
+            ax.add_patch(circle)
+            for theta in thetaR:
+                nu = np.complex(0,theta)
+                rho = (l*np.exp(nu) + (1 - b - l) + a/d* np.exp(-nu) - a/d)/(1-b*np.exp(-nu))
+                x, y = np.real(rho), np.imag(rho)
+                plt.scatter(x,y)
             
-            theta = np.log((l*np.exp(nu) + (1 - b - l) + a/d* np.exp(-nu) - a/d)/(1-b*np.exp(-nu)))
-            equation = abs(is_not_zero(np.imag(theta))) + abs(np.real(theta))
-            plt.figure()
-            plt.contour(x, y, equation, [0])
-            plt.xlabel('$\Re ( \\nu )$')
-            plt.ylabel('$\Im ( \\nu )$')
-            plt.title('$\Im (\\theta) \\neq 0$ & $\Re (\\theta) = 0$ ')
-            plt.text(7,8,f' Beta = {b} \n Lambda = {l} \n Alpha = {a} \n d = {d}')
+            plt.xlabel('$\Re ( \\rho )$')
+            plt.ylabel('$\Im ( \\rho )$')
+            #plt.title('$\Im (\\theta) \\neq 0$ & $\Re (\\theta) = 0$ ')
+            plt.text(0.5,0.5,f' Beta = {b} \n Lambda = {l} \n Alpha = {a} \n d = {d}')
             plt.show()
