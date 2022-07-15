9ca38a6e359a9d47030360b7963f2506bef89f32
Guilhem Fouilhé <g.g.a.fouilhe@gmail.com>
2022-07-15 14:10:55 +0200

    update

--- unidimensional/without torch/main.py
+++ unidimensional/without torch/main.py
@@ -1,17 +1,17 @@
-from os import times
+from matplotlib import projections
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 mpl.style.use('seaborn')
 #params
-b = 0.33
-l = 0.33
+b = 0.3
+l = 0.5
 a = 0.01
 d = 100
 
 timesteps = 500
-J = 100
+J = 500
 
 #init
 e = np.zeros((timesteps,J+1))
@@ -20,16 +20,11 @@ e = np.zeros((timesteps,J+1))
 e_0 = 0
 e[:,0] = np.repeat(e_0,timesteps)
 
-# #feedforward weights for first ff pass
-# w = np.zeros(J+1) #np.random.normal(scale=0.5,size=J+1)
-# #first forward pass
-# for j in range(J):
-#     e[:,j+1] = w[j]*e[:,j]
 
 e[:,J//2] = 1
 
 #Neumann
-e[:,J-1] = e[:,J]
+e[:,J] = e[:,J-1]
 
 
 #PC algorithm
@@ -37,18 +32,36 @@ for n in range(timesteps-1):
     for j in range(1,J):
         e[n+1,j] = b*e[n+1,j-1] + l*e[n,j+1] +(1-b-l)* e[n,j] + a/d * (e[n,j-1] - e[n,j])
     #Neumann
-    e[n+1,J-1] = e[n+1,J] 
+    e[n+1,J] = e[n+1,J-1] 
 
 
 
 plt.figure()
 
 for n in range(timesteps):
-    plt.plot(e[n,:],color = plt.get_cmap('magma')(float(n)/timesteps))
+    if n%5 == 0:
+        plt.plot(e[n,:],color = plt.get_cmap('magma')(float(n)/timesteps))
 plt.ylabel('$\\epsilon$')
 plt.ylim(0,0.1)
 plt.xlim(0,J)
 plt.xlabel(f'Layers (0 < j < {J})')
 plt.title(f'Solution over {timesteps} time iterations')
 plt.text(3/4*J,0.07,f" $\\beta = {b} $\n $\lambda = {l} $\n $\\alpha = {a} $\n $d = {d} $")
+plt.show()
+
+
+ax = plt.figure().add_subplot(projection="3d")
+
+for n in range(timesteps):
+    if n%10 == 0:
+        ax.plot(np.array(range(J+1)),e[n,:],zs = n, zdir ='y',color = plt.get_cmap('magma')(float(n)/timesteps))
+
+ax.set_xlim(0,J)
+ax.set_zlim(0,0.05)
+ax.set_ylim(0,timesteps)
+ax.set_xlabel('j')
+ax.set_ylabel('n')
+ax.set_zlabel('$\epsilon$')
+
+
 plt.show()
