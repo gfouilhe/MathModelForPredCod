099180fbd7de5750650fb54c929b5b91e5695507
Guilhem Fouilhé <g.g.a.fouilhe@gmail.com>
2022-07-21 16:31:03 +0200

    End of internship :(

--- MNIST-14x14-3H-clean/eigenvalues_plot.py
+++ MNIST-14x14-3H-clean/eigenvalues_plot.py
@@ -6,80 +6,59 @@ import os
 
 def main():
 
-    g = []
-    b = []
-    alphaR = [100]
+    gamma = 0.33
+    beta = 0.33
+    alpha = 120
     numberEpochs = 10
 
-    gammaR = list(np.linspace(0,1,10))
-    betaR = list(np.linspace(0,1,10))
+    trained_params_couples = [(0.33,0.33,100)]#[(0.1,0.8), (0.1,0.5), (0.1,0.1),(0.33,0.33),(0.2,0.5),(0.5,0.2),(0.5,0.1),(0.8,0.1)]
 
+    for gammaFw, betaFB, alphaRec in trained_params_couples:
 
-    trained_params_couples = [(0.33,0.33)]#[(0.1,0.8), (0.1,0.5), (0.1,0.1),(0.33,0.33),(0.2,0.5),(0.5,0.2),(0.5,0.1),(0.8,0.1)]
-
-    for gammaFw, betaFB in trained_params_couples:
-
-        for alpha in alphaR:
-
-            print(f'trained in gamma = {gammaFw} and beta = {betaFB} and alpha = {alpha}')
-
-            model= PCMLP(0.33,alpha,betaFB,gammaFw)
-            checkpointPhase = torch.load(os.path.join('models',f"FFREC_E{numberEpochs-1}_I0_G{gammaFw}_B{betaFB}_A{alpha}.pth"))
-            model.load_state_dict(checkpointPhase["module"])
-            for name, p in model.named_parameters():
-                tmp = p.detach().numpy()
-                if name=='fcAB.weight':
-                    W12 = tmp
-                if name=='fcBA.weight':
-                    W21 = tmp
-                if name=='fciA.weight':
-                    W01 = tmp
-                if name=='fcAi.weight':
-                    W10 = tmp
-
-            
-            d = W01.shape[1]
-        
-            if betaFB + gammaFw > 1:
-                pass
-            else:
-                g.append(gammaFw)
-                b.append(betaFB)
-                ax = plt.gca()
-                fig = plt.gcf()
-                ax.cla()
-                circle = plt.Circle((0,0),1,color='r',fill=False)
-                ax.add_patch(circle)
-
-                for gamma in gammaR:
-                    for beta in betaR:
-
-                        if gamma+beta > 1:
-                            pass
-                        else:
-
-                            A11 = (1-beta-gamma) * np.eye(d) - alpha/d * W10.T.dot(W10)
-                            A12 = beta * W21
-                            A21 = (1-beta-gamma) * gamma * W12 - alpha/d * gamma * W12.dot(W10.T.dot(W10)) + alpha/d * W21.T
-                            A22 = gamma * beta * W12.dot(W21) + (1-gamma) * np.eye(d) - alpha/d * W21.T.dot(W21)
-
-                            A = np.block([[A11,A12],[A21,A22]])
-                            w, v = np.linalg.eig(A)
-                    
-                            for eig in w:
-                                re = np.real(eig)
-                                im = np.imag(eig)
-                                ax.plot(re,im,'o',color='blue')
-                                plt.title(f'eigs_G{gammaFw}_B{betaFB}_A{alpha}')
-
-                plt.savefig(os.path.join('eigenvalues_plot',f'eigs_G{gammaFw}_B{betaFB}_A{alpha}.png'))
-                plt.close()
     
-    plt.scatter(g,b)
-    plt.xlim(0,1)
-    plt.ylim(0,1)
-    plt.title('Used parameters')
-    plt.savefig(os.path.join('eigenvalues_plot',f'used_parameters.png'))
+
+        print(f'trained in gamma = {gammaFw} and beta = {betaFB} and alpha = {alphaRec}')
+
+        model= PCMLP(0.33,alphaRec,betaFB,gammaFw)
+        checkpointPhase = torch.load(os.path.join('models',f"FFREC_E{numberEpochs-1}_I0_G{gammaFw}_B{betaFB}_A{alphaRec}.pth"))
+        model.load_state_dict(checkpointPhase["module"])
+
+        for name, p in model.named_parameters():
+            tmp = p.detach().numpy()
+            if name=='fcAB.weight':
+                W12 = tmp
+            if name=='fcBA.weight':
+                W21 = tmp
+            if name=='fciA.weight':
+                W01 = tmp
+            if name=='fcAi.weight':
+                W10 = tmp
+
+        
+        d = W01.shape[1]
+
+        ax = plt.gca()
+        fig = plt.gcf()
+        ax.cla()
+        circle = plt.Circle((0,0),1,color='r',fill=False)
+        ax.add_patch(circle)
+
+    
+        A11 = (1-beta-gamma) * np.eye(d) - alpha/d * W10.T.dot(W10)
+        A12 = beta * W21
+        A21 = (1-beta-gamma) * gamma * W12 - alpha/d * gamma * W12.dot(W10.T.dot(W10)) + alpha/d * W21.T
+        A22 = gamma * beta * W12.dot(W21) + (1-gamma) * np.eye(d) - alpha/d * W21.T.dot(W21)
+
+        A = np.block([[A11,A12],[A21,A22]])
+        w, v = np.linalg.eig(A)
+
+        for eig in w:
+            re = np.real(eig)
+            im = np.imag(eig)
+            ax.plot(re,im,'o',color='blue')
+        plt.title(f'eigs_G{gammaFw}_B{betaFB}_A{alphaRec}_G{gamma}_B{beta}_A{alpha}')
+        plt.savefig(os.path.join('eigenvalues_plot',f'eigs_G{gammaFw}_B{betaFB}_A{alphaRec}_G{gamma}_B{beta}_A{alpha}.png'))
+        plt.close()
 
 if __name__ == "__main__":
     main()
--- MNIST-14x14-3H-clean/oscillations_on_dataset.py
+++ MNIST-14x14-3H-clean/oscillations_on_dataset.py
@@ -1,4 +1,119 @@
 import torch
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from model import PCMLP
+from PIL import Image
+import pickle
+from utils.getdata import get_data
 
 
-i \, \pi - \frac{b \gamma^f \theta^{2}}{2 \, {\left(b^{2} \gamma^f^{2} - 2 \,\beta \gamma^f + 1\right)}} + \frac{i \,\beta \gamma^f \theta}{b \gamma^f - 1} + \frac{{\left(a^{2} \gamma^b^{3} + {\left(l^{2} -\lambda\right)} d^{2} \gamma^b + {\left(\gamma^b^{3}\lambda - 4 \, \gamma^b^{2}\lambda + \gamma^b {\left(l - 1\right)}\right)}\alpha d + {\left(d^{2} \gamma^b\lambda +\alpha d \gamma^b\right)}\beta\right)} \theta^{2}}{2 \, {\left(b^{2} d^{2} + {\left(\gamma^b^{4} - 2 \, \gamma^b^{3} + \gamma^b^{2}\right)}\alpha^{2} - 2 \, {\left(\gamma^b^{3}\lambda - \gamma^b^{2} {\left(2 \,\lambda - 1\right)} + \gamma^b {\left(l - 1\right)}\right)}\alpha d + {\left(\gamma^b^{2}\lambda^{2} - 2 \, {\left(l^{2} -\lambda\right)} \gamma^b +\lambda^{2} - 2 \,\lambda + 1\right)} d^{2} + 2 \, {\left({\left(\gamma^b^{2} - \gamma^b\right)}\alpha d - {\left(\gamma^b\lambda -\lambda + 1\right)} d^{2}\right)}\beta\right)}} + \frac{{\left(-i \, d \gamma^b\lambda + i \,\alpha \gamma^b\right)} \theta}{{\left(\gamma^b^{2} - \gamma^b\right)}\alpha - {\left(\gamma^b\lambda -\lambda + 1\right)} d +\beta d} + \log\left(a \gamma^b^{2} + {\left(b - 1\right)} d -\alpha \gamma^b - {\left(d \gamma^b - d\right)}\lambda\right) - \log\left(-b \gamma^f + 1\right) - \log\left(d\right)
+batchSize = 1
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+
+def main():
+
+    #------ Parameteters -------
+
+    displaymode ='neurons' # 'neurons', 'layernorm'
+     
+    long='' # 'long' = 200 time iterations instead of 50 ('')
+
+    UsedForLearningHyper =  [(0.33,0.33,100)]#,(0.85,0.05,0.01),(0.95,0.01,0.01),(0.7,0.1,0.01)]
+    comment = ''
+    alphaR = [60] #list(np.arange(80,120,5))
+    numberEpochs = 10
+    timeSteps = 200
+    commentact = 'tanh' #'tanh' ; 'relu' 'linear'
+    
+    if commentact == 'linear' :
+        activation = lambda x: x 
+    elif commentact == 'tanh':
+        activation = torch.tanh
+    elif commentact == 'relu':
+        activation = torch.relu
+    
+    _, dataset = get_data()
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batchSize, shuffle=True, num_workers=1)
+    
+
+    for gammaFw, betaFB, alphaRec in UsedForLearningHyper:
+
+        for alpha in alphaR:
+
+            path = os.path.join('parameters_setup',f'{comment}_good_params_dictionary_G{gammaFw}_B{betaFB}_A{alphaRec}.pkl')
+
+            with open(path,'rb') as f:
+                params_and_imgs = pickle.load(f)
+
+            params_list = [param for _,param in params_and_imgs.items()]
+            # params_list = params_list[:20]
+            params_list = [(0.33,0.33,0)]
+            
+            k = 0
+
+            for i,params in enumerate(params_list):
+
+                gamma,beta,_ = params
+
+                for j,data in enumerate(data_loader):
+                    print('new img, ', alpha)
+
+                    if j>10:
+                        break
+
+                    k+=1
+
+                    actA = np.zeros((timeSteps,196))
+                    actB = np.zeros((timeSteps,196))
+                    actO = np.zeros((timeSteps,10))
+
+                    img, label = data
+                    iTemp = img.to(dtype=torch.float32).to(device)
+                    aTemp = torch.zeros(batchSize, 196).to(dtype=torch.float32).cuda()
+                    bTemp = torch.zeros(batchSize, 196).to(dtype=torch.float32).cuda()
+                    oTemp = torch.zeros(batchSize, 10).to(dtype=torch.float32).cuda()
+                    pcmodel = PCMLP(0.33,alphaRec=alpha,betaFB=beta,gammaFw=gamma,activation_function=activation).to(device)
+                    checkpointPhase = torch.load(os.path.join('models',f"FFREC_E{numberEpochs-1}_I0_G{gammaFw}_B{betaFB}_A{alphaRec}.pth"))
+                    pcmodel.load_state_dict(checkpointPhase["module"])
+
+                    outputs, iTemp, aTemp, bTemp, oTemp, reconstruction = pcmodel(iTemp.view(batchSize,-1), aTemp, bTemp, oTemp, 'forward')
+                    _, predicted = torch.max(outputs.data, 1)
+
+                    print('label : ', label)
+                    print('output after first ff pass : ', predicted)
+                    
+                    for t in range(timeSteps):
+                        
+                        outputs, _, aTemp, bTemp, oTemp, _ =  pcmodel(iTemp.view(batchSize,-1), aTemp, bTemp, oTemp, 'full')
+
+                        if t%33==0:
+                            _, predicted = torch.max(outputs.data, 1)
+                            print(f'output  (t = {t}): ', predicted)
+
+                        actA[t,:] = aTemp.detach().cpu().numpy()
+                        actB[t,:] = bTemp.detach().cpu().numpy()
+                        actO[t,:] = oTemp.detach().cpu().numpy()  
+
+
+                    if np.size(actA[:,:])==0:
+                        pass
+                    else:
+                        for j in range(5):
+                                
+                            plt.figure(figsize=(15,5))
+                            plt.subplot(1,2,1)
+                            plt.plot(actA[:,j])
+                            # plt.yscale('log')
+                            plt.title(f'Layer A, neuron {j}')
+                            plt.subplot(1,2,2)
+                            plt.plot(actB[:,j])
+                            # plt.yscale('log')
+                            plt.title(f'Layer B, neuron {j}')
+                            plt.savefig(os.path.join(f'activations_on_data',f'{displaymode}{long}{commentact}actplot_G{gammaFw}_B{betaFB}_A{alphaRec}_G{gamma}_B{beta}_A{alpha}_{k}.png'))
+                            plt.close()
+if __name__ == "__main__":
+    main()
